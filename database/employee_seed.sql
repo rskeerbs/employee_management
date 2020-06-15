8259729ce7b874a26f4d5dc233e1957a8e947252
Romi Keerbs <rskeerbs@gmail.com>
2020-06-14 19:28:38 -0700

    Working copy

--- database/employee_seed.sql
+++ database/employee_seed.sql
@@ -1,16 +1,16 @@
 USE employee_management;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Chance", "Tabby", 1, null);
+VALUES ("Chance", "Tabby", 1, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Sterling", "Silver", 1, null);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Salem", "Black", 1, null);
+VALUES ("Salem", "Black", 1, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Magic", "Black", 1, null);
+VALUES ("Magic", "Black", 1, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Zorro", "Brown", 1, null);
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 const connection = require('./database/connection.js');
-const Font = require('ascii-art-font');
 
 
 function prompts(){
@@ -58,79 +57,125 @@ async function viewRoles(){
 };
 
 async function viewEmployees(){
-    let employees = await connection.query("SELECT * FROM employee;");
+    let employees = await connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name, role.salary, CONCAT(manager.first_name,' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id;");
     console.table(employees);
     prompts();
 }
 
-async function addDepartment(){
+async function addDepartments(){
     inquirer.prompt([{
         type:"input",
-        name:"departmentAdd",
+        name:"department_name",
         message:"What department would you like to add?",
-        }])
-    let department = await connection.query("INSERT INTO department VALUES departmentAdd;;");
-    console.table(department);
-    prompts();
+        }]).then(async function(data){
+            let department = await connection.query("INSERT INTO department SET ?", data);
+            console.log("Your deparment was added.")
+            prompts();
+        })
+    
 }
 
-async function addRole(){
+async function addRoles(){
+    var departments= await connection.query("SELECT * FROM department");
+    var departmentArray= departments.map(function(dept){
+        return dept.id + " - " + dept.department_name;
+    })
     inquirer.prompt([{
         type:"input",
-        name:"addRole",
+        name:"title",
         message:"What role would you like to add?",
         },
         {
         type:"input",
-        name:"addSalary",
-        message:"What is the slary for this role?",
+        name:"salary",
+        message:"What is the salary for this role?",
+        },
+        {
+        type:"list",
+        name:"department_id",
+        message:"What is the department for this role?",
+        choices: departmentArray
         }
             
-    ])
-    let employees = await connection.query("INSERT INTO role VALUES addRole, addSalary;");
-    console.table(employees);
-    prompts();
+    ]).then(async function(data){
+        data.department_id=data.department_id.split(" - ")[0]
+        let employees = await connection.query("INSERT INTO role SET ?", data);
+        console.log("You have added a role.");
+        prompts();
+    })
+    
 }
 
-async function addEmployee(){
+async function addEmployees(){
+    var roles= await connection.query("SELECT * FROM role");
+    var roleArray= roles.map(function(role){
+        return role.id + " - " + role.title;
+    })
+    var managers= await connection.query("SELECT * FROM employee");
+    var managerArray= managers.map(function(mgr){
+        return mgr.id + " - " + mgr.first_name + " " + mgr.last_name;
+    })
     inquirer.prompt([{
         type:"input",
-        name:"addFirst",
+        name:"first_name",
         message:"What is the employee's first name?",
         },
         {
         type:"input",
-        name:"addLast",
+        name:"last_name",
         message:"What is the employee's last name?",
         },
         {
-        type:"input",
-        name:"addEmpDept",
+        type:"list",
+        name:"role_id",
         message:"What is the employee's role?",
+        choices: roleArray
+        },
+        {
+        type:"list",
+        name:"manager_id",
+        message:"Who is the employee's manager?",
+        choices: managerArray
         }
     
     
-    ])
-    let employees = await connection.query("INSERT INTO employee VALUES addFirst, addLast, addEmpDept;;");
-    console.table(employees);
+    ]).then(async function(data){
+        data.role_id=data.role_id.split(" - ")[0]
+        data.manager_id=data.manager_id.split(" - ")[0]
+    let employees = await connection.query("INSERT INTO employee SET ?", data);
+    console.log("The employee was added.");
     prompts();
+    });
 }
 
 async function updateRoles(){
+    var roles= await connection.query("SELECT * FROM role");
+    var roleArray= roles.map(function(role){
+        return role.id + " - " + role.title;
+    })
+    var employees= await connection.query("SELECT * FROM employee");
+    var employeeArray=employees.map(function(emp){
+        return emp.id + " - " + emp.first_name + " " + emp.last_name;
+    })
     inquirer.prompt([{
-        type:"input",
-        name:"employeeUpdate",
+        type:"list",
+        name:"id",
         message:"Which employee do you wish to update the role for?",
+        choices: employeeArray
         },
         {
-        type:"input",
-        name:"changeRole",
+        type:"list",
+        name:"role_id",
         message:"What is the employee's new role?",
+        choices: roleArray
         },
-        ])
-    let departments = await connection.query("UPDATE employee SET role_id WHERE name = employeeUpdate;");
-    console.table(departments);
+        ]).then(async function(data){
+        data.role_id=data.role_id.split(" - ")[0]
+        data.id=data.id.split(" - ")[0]
+    let employee = await connection.query("UPDATE employee SET role_id = ? WHERE id = ?;", [data.role_id, data.id]);
+    console.log("The employee's role has been updated.");
     prompts();
+    });
 };
 
 prompts();
